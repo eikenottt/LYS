eb71bf916a2f7cb03dda8fef34ecdd61fb7c6189
runeeikemo <rune.eikem@gmail.com>
2017-10-26 13:49:30 +0200

    Added SVG function in House5.svg

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    const lSwitch = document.querySelector("img");
+    const overlay = document.querySelector("#switch");
+
+    lSwitch.addEventListener("click", function() {
+        lSwitch.setAttribute("src", "images/switch_on.svg");
+        removeOverlay();
+    });
+
+    function removeOverlay() {
+        overlay.classList.add("fadeOut");
+        setTimeout(function (){overlay.remove()}, 1000);
+    }
+
+    function hover() {
+
+    }
+
+};
